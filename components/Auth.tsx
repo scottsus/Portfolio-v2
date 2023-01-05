0a6d83f216cf57c2b779e07e1abaf9787a9a4c0b
Scott Susanto <scottsus@usc.edu>
2023-01-05 11:43:12 -0800

    form and login are working

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSession, signIn } from 'next-auth/react';
-import { InputComment, LoginButton } from '../styles/guestbookStyles';
+import { LoginButton, LoginText } from '../styles/guestbookStyles';
+import InlineInput from './InlineInput';
 
 export default function Auth() {
   const session = useSession();
-  if (!session) return <InputComment>NOT SIGNED IN</InputComment>;
-  return <LoginButton onClick={() => signIn()}>LOGIN</LoginButton>;
+  useEffect(() => console.log('Session:', session));
+  if (session.status === 'unauthenticated')
+    return (
+      <LoginButton
+        onClick={(e) => {
+          e.preventDefault();
+          signIn('google');
+        }}
+      >
+        <LoginText>LOGIN</LoginText>
+      </LoginButton>
+    );
+  return <InlineInput />;
 }
